721cff39a9ad94d9d4a1859387897368a59f9b64
Mikurmi <104067361+Mikurmi@users.noreply.github.com>
2023-03-09 18:19:43 +0000

    Sesion: añadido de los datos de admin y cliente al iniciar sesion

--- madridSkills/sesion/inicio.php
+++ madridSkills/sesion/inicio.php
@@ -2,9 +2,8 @@
 
 session_start();
 $_SESSION['usuario'] = $usuario;
-/*
-Guardamos la infromacion extra independiente de si es cliente o administrador
+//Guardamos la infromacion extra independiente de si es cliente o administrador
 $_SESSION['extra'] = $extra
-*/
+
 
 ?>
